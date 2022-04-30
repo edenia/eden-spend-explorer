30f56457f20782154a57267e6094bd3cf70d06a3
Xavier Fernandez <xavier@crservers.com>
2022-04-30 15:29:17 -0600

    ci(eosreports): configure development environment

--- utils/meta.mk
+++ utils/meta.mk
@@ -3,10 +3,10 @@
 VERSION ?= $(shell git rev-parse --short HEAD)
 CURRENT_BRANCH ?= $(shell git rev-parse --abbrev-ref HEAD)
 
-IMAGE_NAME_WEBAPP=boilerplate-webapp
-IMAGE_NAME_HAPI=boilerplate-hapi
-IMAGE_NAME_HASURA=boilerplate-hasura
-IMAGE_NAME_WALLET=boilerplate-wallet
+IMAGE_NAME_WEBAPP=eosreports-webapp
+IMAGE_NAME_HAPI=eosreports-hapi
+IMAGE_NAME_HASURA=eosreports-hasura
+IMAGE_NAME_WALLET=eosreports-wallet
 
 DOCKER_REGISTRY=eoscostarica506
 SUBDIRS = webapp hapi hasura wallet
