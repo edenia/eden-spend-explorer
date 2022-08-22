cb55b781417cfe8c26fe7f6b8f9c41d8ffcff780
JAMares <josuegerardojgam885@gmail.com>
2022-08-22 11:53:24 -0600

    feat(hapi): adding folder const

--- hapi/src/utils/updater.util.js
+++ hapi/src/utils/updater.util.js
@@ -1,26 +1,13 @@
+import { CATEGORIES } from '../const'
+
 const isEdenExpense = memoString => {
   let splitMemo = memoString.split(':')
   return splitMemo[0].toLowerCase() === 'eden_expense'
 }
 
 const memoSplit = memoString => {
-  const categories = [
-    'admin',
-    'charity',
-    'development',
-    'dues',
-    'education',
-    'hardware',
-    'infrastructure',
-    'legal',
-    'marketing',
-    'pomelo',
-    'salaries',
-    'software',
-    'travel'
-  ]
   const memoSplit = memoString.split('/')
-  const category = categories.includes(memoSplit[0].toLowerCase())
+  const category = CATEGORIES.includes(memoSplit[0].toLowerCase())
     ? memoSplit[0]
     : 'uncategorized'
   const description = category !== 'uncategorized' ? memoSplit[1] : memoString
