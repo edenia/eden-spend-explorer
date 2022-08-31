8309ba2a52686f289622b84ac419ea12e35cec74
Josue Alvarado Mares <josuegerardojgam885@gmail.com>
2022-08-30 20:07:28 -0600

    fix(hapi): unused files deleted

--- hapi/src/services/hyperion/updaters/index.js
+++ hapi/src/services/hyperion/updaters/index.js
@@ -1,9 +1,4 @@
 const genesisEdenWithdrawUpdater = require('./genesiseden-withdraw.updater')
 const eosioTokenTransferUpdater = require('./eosiotoken-transfer.updater')
-const genesisEdenFundTransferUpdater = require('./genesis-fundtransfer.updater')
 
-module.exports = [
-  genesisEdenWithdrawUpdater,
-  genesisEdenFundTransferUpdater,
-  eosioTokenTransferUpdater
-]
+module.exports = [genesisEdenWithdrawUpdater, eosioTokenTransferUpdater]
