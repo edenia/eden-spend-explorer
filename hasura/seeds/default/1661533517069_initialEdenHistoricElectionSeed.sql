c62ebd1d732838043263a67a045be94d29e3f6ef
warnerHurtado <warner3898@gmail.com>
2022-09-02 17:33:54 -0600

    fix(hapi): change wrong word

--- hasura/seeds/default/1661533517069_initialEdenHistoricElectionSeed.sql
+++
@@ -1,4 +0,0 @@
-SET check_function_bodies = false;
-INSERT INTO public.eden_historic_election (election_round, date_election) VALUES (0, '2021-10-08');
-INSERT INTO public.eden_historic_election (election_round, date_election) VALUES (0, '2022-04-08');
-INSERT INTO public.eden_historic_election (election_round, date_election) VALUES (0, '2021-07-08');
