80c442730a54b15a43160d34ac666b4afd9dbf48
Josue Alvarado Mares <josuegerardojgam885@gmail.com>
2022-10-05 14:46:09 -0600

    fix(webapp): uncomment lines

--- webapp/src/routes/IncomeReport/index.js
+++ webapp/src/routes/IncomeReport/index.js
@@ -15,7 +15,7 @@ import IncomeChart from './IncomeChart'
 import IncomeStackedChart from './IncomeStackedChart'
 import IncomePieChart from './IncomePieChart'
 import IncomeSelect from './IncomeSelect'
-// import IncomeTable from './IncomeTable'
+import IncomeTable from './IncomeTable'
 import styles from './styles'
 
 const useStyles = makeStyles(styles)
@@ -36,8 +36,8 @@ const IncomeReport = () => {
       nextEdenDisbursement,
       showElectionRadio,
       incomeClaimedAndUnclaimedList,
-      totalByCategoryList
-      // percentIncomeList
+      totalByCategoryList,
+      percentIncomeList
     },
     {
       setTypeCurrencySelect,
@@ -214,11 +214,11 @@ const IncomeReport = () => {
           </Typography>
 
           <div id="id-table-container">
-            {/* <IncomeTable
+            <IncomeTable
               data={chartTransactionsList}
               thousandSeparator={thousandSeparator}
               dataPercent={percentIncomeList}
-            /> */}
+            />
           </div>
         </div>
       </div>
