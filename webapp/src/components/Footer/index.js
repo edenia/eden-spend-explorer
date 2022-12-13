c9fb6d7b162a8dc7947dd84fe518a1f9f97a0e9e
Josue Alvarado Mares <josuegerardojgam885@gmail.com>
2022-12-13 14:29:03 -0600

    feat(webapp): move constants to component

--- webapp/src/components/Footer/index.js
+++ webapp/src/components/Footer/index.js
@@ -1,34 +1,16 @@
-import React, { memo, useEffect } from 'react'
+import React, { memo } from 'react'
 import { makeStyles, useTheme } from '@mui/styles'
 import { Typography, Link } from '@mui/material'
 import { useTranslation } from 'react-i18next'
 import { Footer } from '@edenia/ui-kit'
 
 import styles from './styles'
-import { footerItems } from '../../constants'
 const useStyles = makeStyles(styles)
 
-const cleanFooterItems = (footerItem, translation) => {
-  for (let pos = 0; pos < footerItem.length; pos++) {
-    footerItem[pos].links.map(element => {
-      return (element.text = translation(element.text, { ns: 'footer' }))
-    })
-    footerItem[pos].title = translation(footerItem[pos].title, { ns: 'footer' })
-  }
-  return footerItem
-}
-
 const FooterComp = () => {
-  const { t, i18n } = useTranslation('footer')
+  const { t } = useTranslation('footer')
   const classes = useStyles()
   const theme = useTheme()
-  let cleanData = cleanFooterItems(footerItems, t)
-
-  console.log({ footerItems })
-  useEffect(() => {
-    cleanData = cleanFooterItems(footerItems, t)
-    console.log({ cleanData })
-  }, [i18n.language])
 
   return (
     <div className={classes.footerRoot}>
@@ -41,7 +23,7 @@ const FooterComp = () => {
               color={theme.palette.grey[600]}
               display="flex"
             >
-              {/* {t('communityOwnedPublic')}&ensp; */}
+              {t('communityOwnedPublic')}&ensp;
               <Link
                 target="_blank"
                 href="https://edenia.com"
@@ -61,7 +43,54 @@ const FooterComp = () => {
             />
           </div>
         }
-        itemsFooter={cleanData}
+        itemsFooter={[
+          {
+            title: t('firstHeader'),
+            links: [
+              {
+                underline: 'none',
+                ref: 'https://genesis.eden.eoscommunity.org/members',
+                target: '_blank',
+                text: t('community')
+              },
+              {
+                underline: 'none',
+                ref: 'https://genesis.eden.eoscommunity.org/induction',
+                target: '_blank',
+                text: t('membership')
+              },
+              {
+                underline: 'none',
+                ref: 'https://www.notion.so/Getting-an-Invite-2d38947d5be94dcb84dfa1ae48894802',
+                target: '_blank',
+                text: t('getInvite')
+              }
+            ]
+          },
+          {
+            title: t('secondHeader'),
+            links: [
+              {
+                underline: 'none',
+                ref: 'https://www.edenelections.com/',
+                target: '_blank',
+                text: t('electionProcess')
+              },
+              {
+                underline: 'none',
+                ref: 'https://github.com/edenia/Eden',
+                target: '_blank',
+                text: t('github')
+              },
+              {
+                underline: 'none',
+                ref: 'https://github.com/edenia/eden-spend-explorer/issues/new/choose',
+                target: '_blank',
+                text: t('bugFeature')
+              }
+            ]
+          }
+        ]}
       />
     </div>
   )
