d62700dd55e3c97184cac825b4eda3794ee850bc
warnerHurtado <warner3898@gmail.com>
2022-12-22 13:15:12 -0600

    fix(hapi): dfuse on dev error

--- webapp/src/hooks/customHooks/useDelegateReportState.js
+++ webapp/src/hooks/customHooks/useDelegateReportState.js
@@ -118,9 +118,12 @@ const useDelegateReportState = () => {
     const responseInitialData = await loadInitialData({
       election: electionRoundSelect
     })
+
+    console.log(responseInitialData)
+
     const length = responseInitialData.data?.delegateFrontend?.data.length
     const maxLevel =
-      responseInitialData.data?.delegateFrontend?.data.at(-1).delegate_level
+      responseInitialData.data?.delegateFrontend?.data.at(-1)?.delegate_level
     const selectedElectionDate =
       responseInitialData.data?.delegateFrontend?.data.at(-2)
     const delegates =
