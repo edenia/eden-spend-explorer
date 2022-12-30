0dff01e7f8141f74e8c957e7ea02fd2b2331b5d6
warnerHurtado <warner3898@gmail.com>
2022-12-30 10:55:12 -0600

    fix(webapp) fix stakedBar data

--- webapp/src/utils/new-format-objects.js
+++ webapp/src/utils/new-format-objects.js
@@ -152,18 +152,19 @@ export const generateDelegateData = (
 }
 
 export const newDataFormatByTypeDelegate = (incomeList, expenseList) => {
+  console.log(incomeList)
+  console.log(expenseList)
+
   const transactions = []
 
   const resultUncategorizedEOS =
-    incomeList[0]?.eos_claimed ||
-    0 + incomeList[0]?.eos_unclaimed ||
-    0 - expenseList[0]?.eos_categorized ||
-    0
+    (incomeList[0]?.eos_claimed || 0) +
+    (incomeList[0]?.eos_unclaimed || 0) -
+    (expenseList[0]?.eos_categorized || 0)
   const resultUncategorizedUSD =
-    incomeList[0]?.usd_claimed ||
-    0 + incomeList[0]?.usd_unclaimed ||
-    0 - expenseList[0]?.usd_categorized ||
-    0
+    (incomeList[0]?.usd_claimed || 0) +
+    (incomeList[0]?.usd_unclaimed || 0) -
+    (expenseList[0]?.usd_categorized || 0)
 
   transactions.push(
     generateDelegateData(
@@ -182,8 +183,8 @@ export const newDataFormatByTypeDelegate = (incomeList, expenseList) => {
       'Categorized',
       expenseList[0]?.eos_categorized || 0,
       expenseList[0]?.usd_categorized || 0,
-      resultUncategorizedEOS,
-      resultUncategorizedUSD
+      resultUncategorizedEOS || 0,
+      resultUncategorizedUSD || 0
     )
   )
 
