e97f2e4ddcc66b4bd1997edfd6e85d54f81af7e3
JAMares <josuegerardojgam885@gmail.com>
2023-01-03 09:01:58 -0600

    feat(webapp): add graph line component

---
+++ webapp/src/components/LineChartReport/index.js
@@ -0,0 +1,206 @@
+import React, { memo, useCallback, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
+import FileSaver from 'file-saver'
+import { Box } from '@mui/system'
+import { makeStyles } from '@mui/styles'
+import {
+  IconButton,
+  Typography,
+  FormControlLabel,
+  Switch,
+  FormGroup
+} from '@mui/material'
+import TooltipDownload from '@mui/material/Tooltip'
+import DownloadOutlined from '@mui/icons-material/DownloadOutlined'
+import { useTranslation } from 'react-i18next'
+import { useCurrentPng } from 'recharts-to-png'
+import {
+  // XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+  LineChart,
+  Line
+} from 'recharts'
+
+import { formatWithThousandSeparator } from '../../utils/format-with-thousand-separator'
+
+import styles from './styles'
+
+const useStyles = makeStyles(styles)
+
+const RenderChartLegend = ({ data }) => {
+  const classes = useStyles()
+  const { t } = useTranslation()
+
+  return (
+    <div className={classes.chartLegent}>
+      <a key={`key-Un${data.toLocaleLowerCase()}-link-chart`}>
+        <Box className={classes.legentCircle} bgcolor="#f4d35e" />
+        {t(`un${data.toLocaleLowerCase()}`, { ns: 'generalForm' })}
+      </a>
+      <a key={`key-${data}-link-chart`}>
+        <Box className={classes.legentCircle} bgcolor="#ee964b" />
+        {t(`${data.toLocaleLowerCase()}`, { ns: 'generalForm' })}
+      </a>
+      <a key={`key-total-link-chart`}>
+        <Box className={classes.legentCircle} bgcolor="#19647e" />
+        {t('total', { ns: 'generalForm' })}
+      </a>
+    </div>
+  )
+}
+
+RenderChartLegend.propTypes = {
+  data: PropTypes.string
+}
+
+const CustomTooltip = ({ payload = [], label = '', coinType = '' }) => {
+  const { t } = useTranslation()
+  label = label + ''
+  const arrayLabel = label.split(' ')
+
+  return (
+    <div>
+      <strong>
+        {`${t(arrayLabel[0].toLocaleLowerCase(), { ns: 'generalForm' })} ${
+          arrayLabel[1]
+        }`}
+      </strong>
+      {payload &&
+        payload.map((data, i) => (
+          <div key={`${i}-tooltip`}>
+            <div>
+              {i === 0 &&
+                data.payload?.date &&
+                `${
+                  data.payload.date ? t('date', { ns: 'generalForm' }) : ''
+                }: ${
+                  data.payload.date ? data.payload.date.split('T')[0] : ''
+                } `}
+            </div>
+            <div>
+              {`${t(
+                data.payload.category
+                  ? `${data.dataKey
+                      .split('_')[1]
+                      .toLocaleLowerCase()}${data.payload.category.toLocaleLowerCase()}`
+                  : data.dataKey.split('_')[1].toLocaleLowerCase(),
+                { ns: 'generalForm' }
+              )}: ${formatWithThousandSeparator(
+                data.payload[data.dataKey],
+                4
+              )} ${coinType}`}
+            </div>
+          </div>
+        ))}
+    </div>
+  )
+}
+
+CustomTooltip.propTypes = {
+  payload: PropTypes.array,
+  label: PropTypes.any,
+  coinType: PropTypes.string
+}
+
+const LineChartReport = ({
+  data,
+  keyTranslation,
+  pathTranslation,
+  showLegend
+}) => {
+  console.log(data)
+  const classes = useStyles()
+  const [getBarPng, { ref: lineRef }] = useCurrentPng()
+  const { t } = useTranslation()
+  const [selectedUSD, setSelected] = useState(false)
+  const [coinType, setCoinType] = useState('EOS')
+
+  const handleChange = event => {
+    setSelected(event.target.checked)
+  }
+
+  const handleBarDownload = useCallback(async () => {
+    const png = await getBarPng()
+
+    if (png) {
+      FileSaver.saveAs(png, 'line-chart.png')
+    }
+  }, [getBarPng])
+
+  useEffect(() => {
+    selectedUSD ? setCoinType('USD') : setCoinType('EOS')
+  }, [selectedUSD])
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.titleContainer}>
+        <div className={classes.title}>
+          <Typography variant="span">
+            {t(keyTranslation, { ns: pathTranslation })}
+          </Typography>
+          <TooltipDownload title={t('download', { ns: 'common' })}>
+            <IconButton onClick={handleBarDownload}>
+              <DownloadOutlined />
+            </IconButton>
+          </TooltipDownload>
+        </div>
+        <div className={classes.filter}>
+          <FormGroup>
+            <FormControlLabel
+              control={<Switch checked={selectedUSD} onChange={handleChange} />}
+              label={t('switchInput', { ns: 'generalForm' })}
+              labelPlacement="start"
+            />
+          </FormGroup>
+        </div>
+      </div>
+      <div className={classes.chartContainer}>
+        <ResponsiveContainer
+          minWidth={600}
+          width="100%"
+          height={300}
+          marginTop="16px"
+        >
+          <LineChart height={300} data={data} ref={lineRef}>
+            <CartesianGrid stroke="#f0f0f0" />
+            <YAxis
+              tick={{ fontSize: '10px', stroke: '#000', strokeWidth: 0.1 }}
+            />
+            <Tooltip
+              wrapperStyle={{
+                outline: 'none',
+                borderRadius: '4px',
+                backgroundColor: '#F9F9F9',
+                fontSize: '14px',
+                padding: '8px'
+              }}
+              content={<CustomTooltip coinType={coinType} />}
+            />
+            {/* {showLegend && (
+              <Legend content={<RenderChartLegend data={category} />} />
+            )} */}
+
+            <Line
+              type="monotone"
+              dataKey="eos_exchange"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </div>
+    </div>
+  )
+}
+
+LineChartReport.propTypes = {
+  data: PropTypes.array,
+  keyTranslation: PropTypes.string,
+  pathTranslation: PropTypes.string,
+  showLegend: PropTypes.bool
+}
+
+export default memo(LineChartReport)
---
+++ webapp/src/components/LineChartReport/styles.js
@@ -0,0 +1,52 @@
+export default theme => ({
+  root: {
+    margin: theme.spacing(2, 0, 2, 0)
+  },
+  chartContainer: {
+    whiteSpace: 'nowrap',
+    overflowY: 'hidden'
+  },
+  title: {
+    '& span': {
+      fontSize: '18px',
+      fontWeight: 500,
+      lineHeight: 1.56,
+      letterSpacing: '-0.4px'
+    }
+  },
+  filter: {
+    '& span': {
+      fontSize: '12px',
+      fontWeight: 300,
+      lineHeight: 1.33,
+      letterSpacing: '-0.26px'
+    }
+  },
+  titleContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  chartLegent: {
+    display: 'flex',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+    '& a ': {
+      display: 'flex',
+      textAlign: 'left',
+      textDecoration: 'none',
+      fontSize: '12px'
+    },
+    '& a:hover': {
+      pointerEvents: 'none'
+    }
+  },
+  legentCircle: {
+    width: 12,
+    height: 12,
+    marginLeft: '4vw',
+    marginTop: theme.spacing(0.4),
+    marginRight: theme.spacing(1),
+    borderRadius: 5
+  }
+})
