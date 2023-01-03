f37f2541eed775509c214dad708a706e246f30c2
warnerHurtado <warner3898@gmail.com>
2023-01-03 14:40:55 -0600

    refactor(webapp): delete unused code

--- webapp/src/hooks/customHooks/useIncomeReportState.js
+++ webapp/src/hooks/customHooks/useIncomeReportState.js
@@ -1,40 +1,30 @@
 import { useEffect, useState } from 'react'
 
 import {
+  GET_ELECTIONS,
   GET_GENERAL_INCOME,
   GET_DELEGATES_BY_ELECTION_INCOME
-} from '../../gql/income.gql'
-import { GET_ELECTIONS_BY_YEAR } from '../../gql/general.gql'
-import { newDataFormatByDelegatesIncome } from '../../utils/new-format-objects'
-import { useImperativeQuery } from '../../utils'
+} from '../../gql'
+import { useImperativeQuery, newDataFormatByDelegatesIncome } from '../../utils'
 
 const useIncomeReportState = () => {
   const [electionRoundSelect, setElectionRoundSelect] = useState(0)
   const [showElectionRadio, setShowElectionRadio] = useState('')
-  const [electionsByYearList, setElectionsByYearList] = useState([])
+  const [electionsList, setelectionsList] = useState([])
   const [incomeByElectionsList, setIncomeByElectionsList] = useState([])
   const [delegatesList, setDelegatesList] = useState([])
 
   const loadGeneralIncome = useImperativeQuery(GET_GENERAL_INCOME)
-
-  const loadElectionsByYear = useImperativeQuery(GET_ELECTIONS_BY_YEAR)
-
+  const loadElections = useImperativeQuery(GET_ELECTIONS)
   const loadDelegatesByElection = useImperativeQuery(
     GET_DELEGATES_BY_ELECTION_INCOME
   )
 
   useEffect(async () => {
-    const electionsByYearData = await loadElectionsByYear({
-      minDate: `2021-01-01`,
-      maxDate: `${new Date().getFullYear()}-12-31`
-    })
+    const { data: electionsData } = await loadElections()
 
-    setElectionRoundSelect(
-      electionsByYearData.data?.eden_historic_election[0].election
-    )
-    setElectionsByYearList(
-      electionsByYearData.data?.eden_historic_election || []
-    )
+    setElectionRoundSelect(electionsData.eden_election[0]?.election)
+    setelectionsList(electionsData.eden_election || [])
   }, [])
 
   useEffect(async () => {
@@ -58,45 +48,10 @@ const useIncomeReportState = () => {
     }
   }, [showElectionRadio, electionRoundSelect])
 
-  useEffect(async () => {
-    const electionsByYearData = await loadElectionsByYear({
-      minDate: `2021-01-01`,
-      maxDate: `${new Date().getFullYear()}-12-31`
-    })
-
-    setElectionsByYearList([
-      ...electionsByYearList,
-      ...electionsByYearData.data?.eden_historic_election
-    ])
-    setElectionRoundSelect(
-      electionsByYearData.data?.eden_historic_election[0].election
-    )
-    setElectionsByYearList(
-      electionsByYearData.data?.eden_historic_election || []
-    )
-  }, [])
-
-  useEffect(() => {
-    const rounds = []
-    for (let pos = 0; pos < incomeByElectionsList.length; pos++) {
-      const election = Number(
-        incomeByElectionsList[pos].election.charAt(
-          incomeByElectionsList[pos].election.length - 1
-        )
-      )
-      const newElections = electionsByYearList.filter(
-        elec => elec.election === election - 1
-      )
-      rounds.push(newElections[0])
-    }
-    setElectionsByYearList(rounds)
-    setElectionRoundSelect(rounds[0]?.election)
-  }, [incomeByElectionsList])
-
   return [
     {
       incomeByElectionsList,
-      electionsByYearList,
+      electionsList,
       delegatesList,
       electionRoundSelect,
       showElectionRadio
