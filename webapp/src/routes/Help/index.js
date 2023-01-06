769ce27674afcc1501df7d89c8bd05f089d736f9
warnerHurtado <warner3898@gmail.com>
2023-01-06 11:03:07 -0600

    fix(webapp):bad images dimentions on mobile version in help section

--- webapp/src/routes/Help/index.js
+++ webapp/src/routes/Help/index.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 import { makeStyles } from '@mui/styles'
-import { Accordion, AccordionSummary, AccordionDetails } from '@mui/material'
+import {
+  Accordion,
+  AccordionSummary,
+  AccordionDetails,
+  Divider
+} from '@mui/material'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
 import { useTranslation, Trans } from 'react-i18next'
 
@@ -39,16 +44,15 @@ const Help = () => {
           <span className={classes.subTitle}>
             {t('userRoleText', { ns: 'helpRoute' })}
           </span>
-          <img
-            src={
-              i18n.language === 'es'
-                ? '/images/role-access-ES.webp'
-                : '/images/role-access-EN.webp'
-            }
-            width={720}
-            height={268}
-            className={classes.images}
-          />
+          <div className={classes.imagesContainer}>
+            <img
+              src={
+                i18n.language === 'es'
+                  ? '/images/role-access-ES.webp'
+                  : '/images/role-access-EN.webp'
+              }
+            />
+          </div>
         </AccordionDetails>
       </Accordion>
       <Accordion
@@ -70,16 +74,16 @@ const Help = () => {
               {t('balanceTitle', { ns: 'helpRoute' })}
             </span>
           </Trans>
-          <img
-            src={
-              i18n.language === 'es'
-                ? '/images/balance-ES.webp'
-                : '/images/balance-EN.webp'
-            }
-            width={366}
-            height={136}
-            className={classes.imgBalance}
-          />
+          <div className={classes.imagesContainer}>
+            <img
+              id="balance"
+              src={
+                i18n.language === 'es'
+                  ? '/images/balance-ES.webp'
+                  : '/images/balance-EN.webp'
+              }
+            />
+          </div>
           <div className={classes.paragraph}>
             <span className={classes.text}>
               {t('balanceText1', { ns: 'helpRoute' })}
@@ -96,7 +100,7 @@ const Help = () => {
               {t('balanceText2', { ns: 'helpRoute' })}
             </span>
           </div>
-          <div className={classes.horizontalLine} />
+          <Divider orientation="horizontal" variant="middle" />
           <Trans>
             <span className={classes.subTitle}>
               {t('chartsTitle', { ns: 'helpRoute' })}
@@ -109,9 +113,6 @@ const Help = () => {
                   ? '/images/charts-ES.webp'
                   : '/images/charts-EN.webp'
               }
-              width={1100}
-              height={396}
-              className={classes.images}
             />
           </div>
           <div className={classes.paragraph}>
@@ -156,16 +157,16 @@ const Help = () => {
           <span className={classes.subTitle}>
             {t('spendSubTitle', { ns: 'helpRoute' })}
           </span>
-          <img
-            src={
-              i18n.language === 'es'
-                ? '/images/spend-tools-ES.webp'
-                : '/images/spend-tools-EN.webp'
-            }
-            width={720}
-            height={311}
-            className={classes.imgBalance}
-          />
+          <div className={classes.imagesContainer}>
+            <img
+              id="spendTool"
+              src={
+                i18n.language === 'es'
+                  ? '/images/spend-tools-ES.webp'
+                  : '/images/spend-tools-EN.webp'
+              }
+            />
+          </div>
           <div className={classes.paragraph}>
             <Trans>
               <span className={classes.text}>
@@ -183,7 +184,7 @@ const Help = () => {
                 {t('spendSteps2', { ns: 'helpRoute' })}
               </span>
             </Trans>
-            <div className={classes.horizontalLine} />
+            <Divider orientation="horizontal" variant="middle" />
             <Trans>
               <span className={classes.subTitle}>
                 {t('spendSubTitle2', { ns: 'helpRoute' })}
@@ -195,16 +196,16 @@ const Help = () => {
               </div>
             </Trans>
           </div>
-          <img
-            src={
-              i18n.language === 'es'
-                ? '/images/spend-tools2-ES.webp'
-                : '/images/spend-tools2-EN.webp'
-            }
-            width={469}
-            height={341}
-            className={classes.imgBalance}
-          />
+          <div className={classes.imagesContainer}>
+            <img
+              id="categorizedSpend"
+              src={
+                i18n.language === 'es'
+                  ? '/images/spend-tools2-ES.webp'
+                  : '/images/spend-tools2-EN.webp'
+              }
+            />
+          </div>
         </AccordionDetails>
       </Accordion>
     </div>
